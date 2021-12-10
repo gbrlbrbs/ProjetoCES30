9d9e89629be4b87edb9b0c23c3a9e4736da15812
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-12-09 22:07:46 -0300

    encoding

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
     data_path = Path.cwd() / 'data'
     spark = SparkSession.builder.master('local').getOrCreate()
     spark \
-        .read.csv(str(data_path / 'MICRODADOS_ENEM_2019.csv'), sep=";", header=True, inferSchema=True) \
+        .read \
+        .csv(
+            str(data_path / 'MICRODADOS_ENEM_2019.csv'), sep=";",
+            header=True, inferSchema=True, encoding='ISO-8859-1'
+        ) \
         .write.mode('overwrite').parquet(str(data_path / 'microdados.parquet'))
 
 if __name__ == "__main__":
